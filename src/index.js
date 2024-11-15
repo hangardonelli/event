93e667ba2bc2f86507e97fec6ff142c39483bf7b
Lautaro Conde <lautarogconde@gmail.com>
2024-11-15 01:12:42 -0300

    Revert "Remove comments"
    This reverts commit d406875234515af4f4b110c87759ddd1630ba172.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ const { logServerStart } = require('./logger.js');
 const app = express();
 const port = 3000;
 
+// Ruta de prueba
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
 
+// Escuchar en el puerto 3000
 app.listen(port, () => {
   logServerStart(port);
 });
