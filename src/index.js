9e05068759f52630ac3495930387acc822509c0c
Lautaro Conde <lautarogconde@gmail.com>
2024-11-15 20:03:02 -0300

    remove somme comments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const { logServerStart } = require('./logger.js');
-const routes = require('./routes.js'); // Aquí importamos el archivo routes.js
+const routes = require('./routes.js');
 
 const app = express();
 const port = 3000;
 
-// Usamos las rutas definidas en routes.js
 app.use(routes);
-
-app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
-});
 
 app.listen(port, () => {
   logServerStart(port);
