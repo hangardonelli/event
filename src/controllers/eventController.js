9083aea8ebef432566f0f41c38d05b15e8e67bf3
Lautaro Conde <lautarogconde@gmail.com>
2024-11-15 20:06:26 -0300

    mock data changes

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,23 +1,30 @@
 const mockEvents = [
-  {
-    date: "1942-09-15",
-    lat: 48.7236,
-    lon: 44.514,
-    description: "Intense combat in the northern suburbs of Stalingrad.",
-  },
-  {
-    date: "1942-09-17",
-    lat: 48.7123,
-    lon: 44.5221,
-    description: "German forces push deeper into the city.",
-  },
-  {
-    date: "1942-09-20",
-    lat: 48.699,
-    lon: 44.5092,
-    description: "Soviet forces launch counterattacks on the Volga.",
-  },
-];
+    {
+      startDate: "1942-09-15",
+      endDate: null,
+      lat: 48.7236,
+      lon: 44.514,
+      description: "Intense combat in the northern suburbs of Stalingrad.",
+      eventType: "Combat",
+    },
+    {
+      startDate: "1942-09-17",
+      endDate: null,
+      lat: 48.7123,
+      lon: 44.5221,
+      description: "German forces push deeper into the city.",
+      eventType: "Advance",
+    },
+    {
+      startDate: "1942-09-20",
+      endDate: null,
+      lat: 48.699,
+      lon: 44.5092,
+      description: "Soviet forces launch counterattacks on the Volga.",
+      eventType: "Counterattack",
+    },
+  ];
+  
 
 const getEventsByDate = (req, res) => {
   const { date } = req.params;
