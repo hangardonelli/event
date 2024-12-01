cecc5fa628aef93a5a35ff2ae7d13189f4e0be33
Lautaro Conde <lautarogconde@gmail.com>
2024-12-01 02:49:15 -0300

    Prettier on index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import routes from "./routes.js";
 import logger from "./config/logger.mjs";
 import dotenv from "dotenv";
 import { ENVIROMENTS } from "./config/enviroments.js";
-import {getKeyByValue} from "./utils/utils.js";
+import { getKeyByValue } from "./utils/utils.js";
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
-const isProduction = process.env.ENVIROMENT === getKeyByValue(ENVIROMENTS, ENVIROMENTS.PROD);
+const isProduction =
+  process.env.ENVIROMENT === getKeyByValue(ENVIROMENTS, ENVIROMENTS.PROD);
 
 app.use(routes);
 
 app.listen(port, () => {
   logServerStart(port);
-  logger.info(`Server running at ${`http://localhost:${port}`} 🚀`, isProduction);
+  logger.info(
+    `Server running at ${`http://localhost:${port}`} 🚀`,
+    isProduction
+  );
 });
